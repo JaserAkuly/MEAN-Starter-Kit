9588583d547b46dc4c8033fa3e6daf01fc001ccf
Jaser Akuly <jaser1992@gmail.com>
2015-11-05 17:26:27 -0600

    Creating a Layout.

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
  * Created by jaser.akuly on 11/5/15.
  */
 var express = require('express');
+    stylus = require('stylus');
 
 var env  = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
