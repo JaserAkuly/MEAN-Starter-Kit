ce49bd30955f81c8d4ad761b77011e59f8a172b8
Jaser Akuly <jaser1992@gmail.com>
2015-11-05 01:18:52 -0600

    Hello World Commit
    Completed initial Walking Skeleton “Hello World” testing. Please see
    Readme to get setup.

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jaser.akuly on 11/5/15.
+ */
+var express = require('express');
+
+var env  = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+var port = 3000;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
